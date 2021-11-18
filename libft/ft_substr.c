e254c6593c812ce3a48811299c6c5d6ba9303f97
Jaime Velasco Bustamante <jvelasco@student.42madrid.com>
2021-11-18 18:49:04 +0100

    substrs fix buffer

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jvelasco <jvelasco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 15:39:25 by jvelasco          #+#    #+#             */
-/*   Updated: 2021/11/18 10:05:33 by jvelasco         ###   ########.fr       */
+/*   Updated: 2021/11/18 18:40:15 by jvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*news;
 	int		i;
-	int		lens;
 
 	if (!s)
 		return (0);
 	i = 0;
-	lens = ft_strlen(s);
+	if (start >= (unsigned int)ft_strlen(s))
+		return (ft_strdup(""));
 	news = malloc(len + 1);
 	if (!news)
 		return (NULL);
-	while (i < (int)len && start < (unsigned int)lens)
+	while (s[i] && i < (int)len)
 		news[i++] = s[start++];
 	news[i] = 0;
 	return (news);
